a42ec703ebee79f7ea5dead5b34463798d2dc5c8
Ignacio <ignacio.evj@gmail.com>
2023-11-27 22:08:46 -0300

    intendanto mostrar datos

--- Frontend/src/components/TablaUsuarios.tsx
+++ Frontend/src/components/TablaUsuarios.tsx
@@ -1,33 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-interface Usuario {
-  id: number;
-  nombre: string;
-}
+function TablaUsuarios() {
+  const [usuarios, setUsers] = useState<{ id_user: number, firstName: string, email: string }[]>([
+    { id_user: 1, firstName: 'Prueba', email: 'prueba@example.com' },
+    // Agrega aquí más usuarios de prueba si necesitas
+  ]);
+  useEffect(() => {
+    fetch('http://localhost:3000/users')
+      .then(response => response.json())
+      .then(data => setUsers(data))
+      .catch(error => console.error('Error:', error));
+  }, []);
 
-interface TablaUsuariosProps {
-  usuarios: Usuario[];
-}
-
-const TablaUsuarios: React.FC<TablaUsuariosProps> = ({ usuarios }) => {
   return (
-    <table className='text-white'>
+    <table>
       <thead>
         <tr>
           <th>ID</th>
           <th>Nombre</th>
+          <th>Email</th>
+          {/* Agrega aquí más columnas si necesitas */}
         </tr>
       </thead>
       <tbody>
-        {usuarios.map((usuario) => (
-          <tr key={usuario.id}>
-            <td>{usuario.id}</td>
-            <td>{usuario.nombre}</td>
+        {usuarios.map(usuario => (
+          <tr key={usuario.id_user}>
+            <td>{usuario.id_user}</td>
+            <td>{usuario.firstName}</td>
+            <td>{usuario.email}</td>
+            {/* Agrega aquí más celdas si necesitas */}
           </tr>
         ))}
       </tbody>
     </table>
   );
-};
+}
 
 export default TablaUsuarios;
